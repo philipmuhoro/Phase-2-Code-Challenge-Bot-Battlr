b62ccd67171241f51703c237163ce64becb80e03
philipwanjiku <muhorophilip7@gmail.com>
2023-11-20 16:06:21 +0300

    create Bot collection

---
+++ bot-battlr/src/BotCollection.js
@@ -0,0 +1,8 @@
+// BotCollection.js
+import React from 'react';
+import './BotCollection.css';
+
+const BotCollection = ({ bots, onAddToArmy }) => {
+  return (
+    <div className="BotCollection">
+      <h2>Bot Collection</h2>
