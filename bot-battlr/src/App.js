77d56447c9c89b4ca39c552285279488b5a2e451
philipmuhoro <145478295+philipmuhoro@users.noreply.github.com>
2023-11-20 16:48:13 +0300

    Update App.js

--- bot-battlr/src/App.js
+++ bot-battlr/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [army, setArmy] = useState([]);
 
   useEffect(() => {
-    fetch(''https://my-json-server.typicode.com/philipmuhoro/Phase-2-Code-Challenge-Bot-Battlr/bots'') // Fetch bots from the backend API.
+    fetch('http://localhost:3000/bots') // Fetch bots from the backend API.
       .then((response) => response.json())
       .then((data) => setBots(data))
       .catch((error) => console.error('Error fetching bots', error));
@@ -26,7 +26,7 @@ const App = () => {
   };
  // Remove the bot from the backend API
  const dischargeBot = (botId) => {
-  fetch(`'https://my-json-server.typicode.com/philipmuhoro/Phase-2-Code-Challenge-Bot-Battlr/bots'${botId}`, {
+  fetch(`http://localhost:3000/bots${botId}`, {
     method: 'DELETE',
   })
     .then((response) => response.json())
